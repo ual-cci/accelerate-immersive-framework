a3015359d35f9ce8302ef79712dfc45bd5f529e8
LouisMcCallum <louis.mccallum@heresyai.com>
2018-05-30 14:22:11 +0100

    gitignore and oauth server

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -3,9 +3,13 @@ var express = require('express');
 var ShareDB = require('sharedb');
 var WebSocket = require('ws');
 var WebSocketJSONStream = require('websocket-json-stream');
+bodyParser = require('body-parser'),
+oauthserver = require('oauth2-server'),
+mongoose = require('mongoose');
 
 const db = require('sharedb-mongo')('mongodb://localhost:27017/test');
 const backend = new ShareDB({db:db});
+const app = express();
 createDoc(startServer);
 
 // Create initial document then fire callback
@@ -25,9 +29,37 @@ function createDoc(callback) {
   });
 }
 
+function startAuth()
+{
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  var mongoUri = 'mongodb://localhost/oauth';
+  mongoose.connect(mongoUri, function(err, res) {
+    if (err) {
+      return console.error('Error connecting to "%s":', mongoUri, err);
+    }
+    console.log('Connected successfully to "%s"', mongoUri);
+  });
+
+  app.oauth = oauthserver({
+    model: require('./model.js'),
+    grants: ['password'],
+    debug: true
+  });
+
+  app.all('/oauth/token', app.oauth.grant());
+
+  app.get('/', app.oauth.authorise(), function (req, res) {
+    res.send('Congratulations, you are in a secret area!');
+  });
+
+  app.use(app.oauth.errorHandler());
+
+}
+
 function startServer() {
   // Create a web server to serve files and listen to WebSocket connections
-  var app = express();
   app.use(express.static('static'));
   var server = http.createServer(app);
 
@@ -42,6 +74,7 @@ function startServer() {
     backend.listen(stream);
   });
 
+  startAuth();
 
   server.listen(8080);
   console.log('Listening on http://localhost:8080');
