a93d64328bf7b8ed2008788072d66364cf1480d4
LouisMcCallum <louis.mccallum@heresyai.com>
2018-06-14 10:27:08 +0100

    stop transition to documents when loaded user, fixes browser paste issue

--- emberShareDB/app/routes/application.js
+++ emberShareDB/app/routes/application.js
@@ -3,9 +3,13 @@ import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Route.extend(ApplicationRouteMixin, {
+  activate() {
+    console.log('entering application route');
+  },
   sessionAccount: inject('session-account'),
   session: inject('session'),
   beforeModel() {
+    console.log('beforeModel application route');
      this._loadCurrentUser();
   },
   sessionAuthenticated() {
@@ -13,9 +17,10 @@ export default Route.extend(ApplicationRouteMixin, {
     this._loadCurrentUser();
   },
   _loadCurrentUser() {
+    console.log('loading curret user');
     this.get('sessionAccount').loadCurrentUser()
     .then(() => {
-      this.transitionTo('documents'," ","0");
+      //this.transitionTo('documents'," ","0");
     })
     .catch(() => {
       this.get('session').invalidate();
