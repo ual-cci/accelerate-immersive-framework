eb00e5c18bb1f5a8bc910f0482e839fbe42e410e
LouisMcCallum <l.mccallum@gold.ac.uk>
2018-06-26 16:51:13 +0100

    basic if statements and functions

--- emberShareDB/app/services/code-parsing.js
+++ emberShareDB/app/services/code-parsing.js
@@ -50,8 +50,99 @@ export default Service.extend({
   },
   insert(source, item)
   {
-    //console.log("inserting", item);
     return source + "\n" + item;
+  },
+  parseNode(node, savedVals, script)
+  {
+    console.log(node);
+    let newSource = "";
+    let added = false;
+    if(node.type = "VariableDeclaration"  && node.declarations)
+    {
+      console.log("VariableDeclaration");
+      newSource = this.insert(newSource,node.kind+" ");
+      for(var i = 0; i < node.declarations.length; i++)
+      {
+        const dec = node.declarations[i];
+        const name = dec.id.name;
+        const init = dec.init;
+        const savedVal = savedVals[name];
+        const delim = i >= node.declarations.length - 1 ? ";" : ","
+        let exp = script.script.substring(dec.start, dec.end) + delim;
+        if(name.substring(0,2) == "p_" && !init && savedVal)
+        {
+          exp = name + " = " + savedVal + delim;
+        }
+        newSource = newSource + exp;
+        added = true;
+      }
+    }
+    else if (node.expression)
+    {
+      if(node.expression.type == "AssignmentExpression")
+      {
+        console.log("AssignmentExpression");
+        const exp = script.script.substring(node.start, node.end);
+        newSource = this.insert(newSource,exp);
+        const name = node.expression.left.name;
+        const msg = "parent.postMessage([\"" + name + "\"," + name + "], \"*\");"
+        newSource = this.insert(newSource, msg);
+        added = true;
+      }
+    }
+    else if(node.params && node.body)
+    {
+      console.log("FunctionDeclaration");
+      newSource = this.insert(newSource, "function " + node.id.name + "(");
+      if(node.params.length > 0)
+      {
+        for(var i = 0; i < node.params.length; i++)
+        {
+          newSource = newSource + node.params[i].name;
+          if(i < node.params.length - 1)
+          {
+            newSource = newSource + ",";
+          }
+        }
+      }
+      newSource = newSource + ") {\n";
+      for(var i = 0; i < node.body.body.length; i++)
+      {
+        newSource = newSource + this.parseNode(node.body.body[i], savedVals, script);
+      }
+      newSource = newSource + "\n}"
+      added = true;
+    }
+    else if(node.body)
+    {
+      console.log("OtherBody");
+      for(var i = 0; i < node.body.length; i++)
+      {
+        newSource = newSource + this.parseNode(node.body[i], savedVals, script);
+      }
+      added = true;
+    }
+    else if (node.consequent)
+    {
+      console.log("consequent");
+      const test = "if(" + script.script.substring(node.test.start, node.test.end) + ") {\n";
+      newSource = this.insert(newSource, test);
+      newSource = newSource + this.parseNode(node.consequent, savedVals, script);
+      newSource = newSource + "\n}"
+      added = true;
+    }
+    else if (node.alternate)
+    {
+      console.log("alternate");
+      newSource = newSource + this.parseNode(node.alternate, savedVals, script);
+      added = true;
+    }
+    if(!added)
+    {
+      const exp = script.script.substring(node.start, node.end);
+      newSource = this.insert(newSource, exp);
+    }
+    return newSource;
   },
   insertStatefullCallbacks(source, savedVals) {
     let newSource = "";
@@ -60,47 +151,17 @@ export default Service.extend({
     {
       const script  = scripts[i];
       newSource = newSource + script.preamble;
-      const parsed = acorn.parse(script.script);
-      for(var j = 0; j < parsed.body.length; j++)
+      let parsed;
+      try {
+        parsed = acorn.parse(script.script);
+      } catch (err) {
+        console.log("Error parsing script", script);
+      }
+      if(parsed)
       {
-        let added = false;
-        const item = parsed.body[j];
-        //console.log(item);
-        if(item.type = "VariableDeclaration"  && item.declarations)
+        for(var j = 0; j < parsed.body.length; j++)
         {
-          newSource = this.insert(newSource,item.kind+" ");
-          for(var k = 0; k < item.declarations.length; k++)
-          {
-            const dec = item.declarations[k];
-            const name = dec.id.name;
-            const init = dec.init;
-            const savedVal = savedVals[name];
-            let exp = script.script.substring(dec.start, dec.end);
-            if(name.substring(0,2) == "p_" && !init && savedVal)
-            {
-              const delim = k >= item.declarations.length - 1 ? ";" : ","
-              exp = name + " = " + savedVal + delim;
-            }
-            newSource = newSource + exp;
-            added = true;
-          }
-        }
-        else if (item.type = "VariableDeclaration" && item.expression)
-        {
-          if(item.expression.type == "AssignmentExpression")
-          {
-            const exp = script.script.substring(item.start, item.end);
-            newSource = this.insert(newSource,exp);
-            const name = item.expression.left.name;
-            const msg = "parent.postMessage([\"" + name + "\"," + name + "], \"*\");"
-            newSource = this.insert(newSource, msg);
-            added = true;
-          }
-        }
-        if(!added)
-        {
-          const exp = script.script.substring(item.start, item.end);
-          newSource = this.insert(newSource, exp);
+          newSource = newSource + this.parseNode(parsed.body[j], savedVals, script);
         }
       }
       newSource = newSource + script.post;
