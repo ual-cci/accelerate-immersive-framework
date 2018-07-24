9d11950fcd7edf3121a8dafa63feb99f57984fb9
Matthew Yee-King <matthew@yeeking.net>
2018-07-24 14:19:39 +0100

    added some websocket test code for server debugging

---
+++ websocket_tests/ws_server.js
@@ -0,0 +1,11 @@
+const WebSocket = require('ws');
+ 
+const wss = new WebSocket.Server({ port: 8080 });
+ 
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('server received: %s', message);
+  });
+ 
+  ws.send('hello from server');
+});
