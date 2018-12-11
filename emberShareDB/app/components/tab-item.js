30c17f3dd4adfddbfe4be7231cfb1ec66b431f6c
LouisMcCallum <l.mccallum@gold.ac.uk>
2018-12-11 14:30:27 +0000

    tab colors

--- emberShareDB/app/components/tab-item.js
+++ emberShareDB/app/components/tab-item.js
@@ -1,6 +1,11 @@
 import Component from '@ember/component';
-
+import { computed } from '@ember/object';
 export default Component.extend({
+  didReceiveAttrs() {
+    this._super(...arguments);
+    const tabid ="tab" + (this.get('tabIndex')+1) % 4;
+    this.set('tabID', tabid)
+  },
   actions: {
     onSelect() {
       console.log("tab item selected", this.get('id'))
