c87b57c56849869457a507fd5f825947e7780394
LouisMcCallum <l.mccallum@gold.ac.uk>
2018-12-18 13:58:29 +0000

    responsive design

---
+++ emberShareDB/app/services/media-queries.js
@@ -0,0 +1,10 @@
+import MediaQueriesService from 'ember-cli-media-queries/services/media-queries';
+
+export default MediaQueriesService.extend({
+  media: {
+    mobile:      '(max-width: 768px)',
+    tablet:      '(min-width: 769px) and (max-width: 992px)',
+    desktop:     '(min-width: 993px) and (max-width: 1200px)',
+    jumbo:       '(min-width: 1201px)',
+  },
+});
