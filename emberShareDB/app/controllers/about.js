72e0be166c99a75dfddff092baa96e645fa10a26
LouisMcCallum <l.mccallum@gold.ac.uk>
2019-03-05 16:51:39 +0000

    responsive title page

--- emberShareDB/app/controllers/about.js
+++ emberShareDB/app/controllers/about.js
@@ -1,8 +1,9 @@
 import Controller from '@ember/controller';
 import config from  '../config/environment';
-
+import { inject } from '@ember/service';
 export default Controller.extend({
   url:config.localOrigin + "/images",
   docURL:config.localOrigin + "/d/ /0/views",
   termsURL:config.localOrigin + "/terms",
+  mediaQueries:inject()
 });
