08dd3922d6473c4dfc4cca93f135328cbec2638d
LouisMcCallum <l.mccallum@gold.ac.uk>
2019-03-20 15:21:00 +0000

    change colour on reload

--- emberShareDB/app/controllers/about.js
+++ emberShareDB/app/controllers/about.js
@@ -1,9 +1,15 @@
 import Controller from '@ember/controller';
 import config from  '../config/environment';
 import { inject } from '@ember/service';
+
 export default Controller.extend({
   url:config.localOrigin + "/images",
   docURL:config.localOrigin + "/d/ /0/views",
   termsURL:config.localOrigin + "/terms",
-  mediaQueries:inject()
+  mediaQueries:inject(),
+  actions:{
+    refresh() {
+      
+    }
+  }
 });
