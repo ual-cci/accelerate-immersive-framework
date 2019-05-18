27cdbd8b69f4a76ab25c1cceb730bd2b9de70d0b
LouisMcCallum <l.mccallum@gold.ac.uk>
2019-05-18 17:05:55 +0100

    get rapid mix guide back online

--- emberShareDB/app/services/guides.js
+++ emberShareDB/app/services/guides.js
@@ -5,8 +5,8 @@ export default Service.extend({
   guides:[
     {id:"mmll", name:"Musical Machine Listening with MMLL.js", url:config.localOrigin + "/guides/mmll"},
     {id:"maximJS", name:"Making Music in the Browser with maxmilian.js", url:config.localOrigin + "/guides/maximJS"},
-    {id:"rapidLib", name:"Using RapidLib for Machine Learning", url:config.localOrigin + "/guides/rapidlib"},
     {id:"kadenze", name:"Machine Learning for Musicians and Artists", url:config.localOrigin + "/guides/kadenze"},
-    {id:"evolib", name:"Evolutionary Sound Synthesis with Evolib", url:config.localOrigin + "/guides/evolib"}
+    {id:"evolib", name:"Evolutionary Sound Synthesis with Evolib", url:config.localOrigin + "/guides/evolib"},
+    {id:"RAPIDMIX", name:"Using RapidLib for Machine Learning", url:config.localOrigin + "/guides/RAPIDMIX"}
   ]
 });
