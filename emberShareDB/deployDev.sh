1eddcc15a91a052271c604a54233779bd8b51586
LouisMcCallum <l.mccallum@gold.ac.uk>
2020-05-21 18:46:58 +0100

    improve build scripts

--- emberShareDB/deployDev.sh
+++ emberShareDB/deployDev.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 FILE="/Users/louismccallum/Documents/programming/MIMIC/Website/MIMIC-editor/emberShareDB/public/libs/maxiSynthProcessor.v.0.5.js"
 tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
-(echo "import Maximilian from \"https://mimicproject.com/libs/maximilian.wasmmodule.v.0.3.js\"" && cat $FILE) > $FILE.tmp && mv $FILE.tmp $FILE
-ember build --environment firebase
+(echo "import Maximilian from \"https://dev.codecircle.gold.ac.uk/libs/maximilian.wasmmodule.v.0.3.js\"" && cat $FILE) > $FILE.tmp && mv $FILE.tmp $FILE
+ember build --environment development
 tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
 (echo "import Maximilian from \"http://localhost:4200/libs/maximilian.wasmmodule.v.0.3.js\"" && cat $FILE) > $FILE.tmp && mv $FILE.tmp $FILE
-rm -rf firebase/public
-mkdir firebase/public
-cp -a dist/ firebase/public
-cd firebase/
-firebase deploy
