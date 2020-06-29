f1b77c663928bef588584b5ff49143f7dc8c056b
LouisMcCallum <l.mccallum@gold.ac.uk>
2020-06-29 15:06:16 +0100

    lazy load the embedded porjects

--- emberShareDB/app/components/embedded-project.js
+++ emberShareDB/app/components/embedded-project.js
@@ -4,5 +4,31 @@ import config from  '../config/environment';
 export default Component.extend({
   docId:"",
   url:config.localOrigin,
-  height:"440px"
-});
+  height:"440px",
+  loaded:false,
+  srcURL:"about:none",
+  didInsertElement() {
+    this._super(...arguments);
+    console.log('element id: ' + this.elementId);
+    this.observe()
+  },
+  observe:function() {
+    var options = {
+      root:document.querySelector('#scrollArea'),
+    }
+
+    var observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+
+        if(!this.get('loaded') &&  entry.intersectionRatio > 0) {
+          let src = this.get("url") + "/code/" + this.get("docId") +  "?embed=true&showCode=true";
+          console.log("onscreen", src);
+          this.set("srcURL", src)
+          this.set("loaded", true);
+        }
+      });
+    }, options);
+
+    observer.observe(document.getElementById(this.elementId));
+ }
+ });
