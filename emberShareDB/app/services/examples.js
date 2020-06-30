7e10dad16ea5afb3a6431b5c9f00d40e7830c2d5
LouisMcCallum <l.mccallum@gold.ac.uk>
2020-06-30 01:26:02 +0100

    reorder exmaples

--- emberShareDB/app/services/examples.js
+++ emberShareDB/app/services/examples.js
@@ -3,12 +3,6 @@ import { computed } from '@ember/object';
 
 export default Service.extend({
   examples:computed(()=>{return [
-    {title:"Audio Remix",examples:[
-      //LSTM
-      {id:"magnet", docid:"84bf177b-af84-85c3-4933-32076561aca0",desc:" This demonstrates an LSTM audio generation process using MAGNet, a spectral approach to audio analysis and generation with neural networks. The techniques included here were used as part of the Mezzanine Vs. MAGNet project featured as part of the Barbican's AI: More than Human exhibition. Here you can try out some pre-trained models. "},
-      //bbcut
-      {id:"bbcut",docid:"50568259-fd05-e122-aa39-d3e39e39a6c0",desc:"BBCut is a remix library based on MMLL.js for beat tracking. The input is cut up live into stuttering buffers, with the cut points determined by tracking of the primary metrical level in the music."},
-    ]},
     {title:"Generative Music", examples:[
       //mario
       {id:"mario",docid:"a8baea19-711f-4e43-46ab-71e5212ed5db",desc:"What if AI finished the Mario theme song? Use Magenta's MusicRNN model to experiment with different continuations of the catchiest computer game music"},
@@ -20,10 +14,17 @@ export default Service.extend({
     {title:"User Input",examples:[
       //audio triggers
       {id:"audio-trigger", docid:"585ecea5-4841-6ae5-a8c1-4a0ddb8975fd",desc:"Train an audio classifier on your own percussive sounds and use it to reconfigure music tracks live."},
-      //shelly drum machine
-      {id:"rhythm-remixer", docid:"c89860a4-824a-5dfc-ff90-412f58531f5a",desc:"Play in rhythms using your microphone and have them remixed into new beats."},
       //FaceAPI
       {id:"facesynth", docid:"9dc4eaf4-93db-26e8-9357-155eb152ea44",desc:"Train a granular sythesiser to respond to the emotions of your face."},
+      //shelly drum machine
+      {id:"rhythm-remixer", docid:"c89860a4-824a-5dfc-ff90-412f58531f5a",desc:"Play in rhythms using your microphone and have them remixed into new beats."},
+
+    ]},
+    {title:"Audio Remix",examples:[
+      //LSTM
+      {id:"magnet", docid:"84bf177b-af84-85c3-4933-32076561aca0",desc:" This demonstrates an LSTM audio generation process using MAGNet, a spectral approach to audio analysis and generation with neural networks. The techniques included here were used as part of the Mezzanine Vs. MAGNet project featured as part of the Barbican's AI: More than Human exhibition. Here you can try out some pre-trained models. "},
+      //bbcut
+      {id:"bbcut",docid:"50568259-fd05-e122-aa39-d3e39e39a6c0",desc:"BBCut is a remix library based on MMLL.js for beat tracking. The input is cut up live into stuttering buffers, with the cut points determined by tracking of the primary metrical level in the music."},
     ]},
     {title:"Synthesis", examples:[
       //evolib
