ff2b3b1b9143c7ee2e5bc20d6c319dcd43239ca4
LouisMcCallum <l.mccallum@gold.ac.uk>
2020-07-01 15:15:09 +0100

    update dev deploy for v.5

--- emberShareDB/deployDev.sh
+++ emberShareDB/deployDev.sh
@@ -5,3 +5,9 @@ tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
 ember build --environment development
 tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
 (echo "import Maximilian from \"http://localhost:4200/libs/maximilian.wasmmodule.v.0.3.js\"" && cat $FILE) > $FILE.tmp && mv $FILE.tmp $FILE
+FILE="$PWD/public/libs/maxiSynthProcessor.v.0.5.js"
+tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
+(echo "import Maximilian from \"https://dev.codecircle.gold.ac.uk/libs/maximilian.wasmmodule.v.0.3.js\"" && cat $FILE) > $FILE.tmp && mv $FILE.tmp $FILE
+ember build --environment development
+tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
+(echo "import Maximilian from \"http://localhost:4200/libs/maximilian.wasmmodule.v.0.3.js\"" && cat $FILE) > $FILE.tmp && mv $FILE.tmp $FILE
