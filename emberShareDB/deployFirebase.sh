83ed50d05c7e94198954f25815d74b2c9c60166f
LouisMcCallum <l.mccallum@gold.ac.uk>
2020-07-10 18:45:35 +0100

    fix deploy script

--- emberShareDB/deployFirebase.sh
+++ emberShareDB/deployFirebase.sh
@@ -2,9 +2,14 @@
 FILE="$PWD/public/libs/maxiSynthProcessor.v.0.4.js"
 tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
 (echo "import Maximilian from \"https://mimicproject.com/libs/maximilian.wasmmodule.v.0.3.js\"" && cat $FILE) > $FILE.tmp && mv $FILE.tmp $FILE
+FILE2="$PWD/public/libs/maxiSynthProcessor.v.0.5.js"
+tail -n +2 "$FILE2" > "$FILE2.tmp" && mv "$FILE2.tmp" "$FILE2"
+(echo "import Maximilian from \"https://mimicproject.com/libs/maximilian.wasmmodule.js\"" && cat $FILE2) > $FILE2.tmp && mv $FILE2.tmp $FILE2
 ember build --environment firebase
 tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
 (echo "import Maximilian from \"http://localhost:4200/libs/maximilian.wasmmodule.v.0.3.js\"" && cat $FILE) > $FILE.tmp && mv $FILE.tmp $FILE
+tail -n +2 "$FILE2" > "$FILE2.tmp" && mv "$FILE2.tmp" "$FILE2"
+(echo "import Maximilian from \"http://localhost:4200/libs/maximilian.wasmmodule.js\"" && cat $FILE2) > $FILE2.tmp && mv $FILE2.tmp $FILE2
 rm -rf firebase/public
 mkdir firebase/public
 cp -a dist/ firebase/public
