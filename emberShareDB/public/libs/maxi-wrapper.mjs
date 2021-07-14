7fec2f0524e9974f37fed64870b2d8b02523c3a1
LouisMcCallum <l.mccallum@gold.ac.uk>
2021-07-14 11:51:06 +0100

    update sema -engine and maxi wrapper

--- emberShareDB/public/libs/maxi-wrapper.mjs
+++ emberShareDB/public/libs/maxi-wrapper.mjs
@@ -1,14 +1,13 @@
 
-// import {
-//   Engine,
-//   Learner,
-// } from "http://localhost:4200/libs/sema-engine.mjs";
-// const origin = "http://localhost:4200/libs";
 import {
   Engine,
   Learner,
-} from "https://mimicproject.com/libs/sema-engine.mjs";
+} from "./sema-engine.mjs";
+
+import RingBuffer from "./ringbuf.js";
+
 const origin = "https://mimicproject.com/libs";
+//const origin = "http://localhost:4200/libs";
 
 export let maxi;
 let dspCode = "";
@@ -18,8 +17,7 @@ maxi.play();
 let learner = new Learner();
 maxi.addLearner('l1', learner);
 dspCode = {
-setup: `() => {
-	let q=this.newq();
+setup: `() => {let q=this.newq();
   let createDSPLoop = ()=> {
     return ()=>{return 0}
   }
@@ -29,22 +27,38 @@ setup: `() => {
 loop: `(q, inputs, mem) => {this.dacOutAll(q.play());}`
 }
 
+maxi.addEventListener('onSharedBuffer', (e) => {
+    console.log(e)
+    let ringbuf = new RingBuffer(e.sab, Float64Array);
+    maxi.sharedArrayBuffers[e.channelID] = {
+      sab: e.sab,
+      rb: ringbuf,
+      blocksize:e.blocksize
+    };
+});
+
 function sabPrinter() {
   try {
     for (let v in maxi.sharedArrayBuffers) {
       let avail = maxi.sharedArrayBuffers[v].rb.available_read();
+      var rd = Atomics.load(maxi.sharedArrayBuffers[v].rb.read_ptr, 0);
+      var wr = Atomics.load(maxi.sharedArrayBuffers[v].rb.write_ptr, 0);
+      //console.log(v, rd, wr)
       if ( avail > 0 && avail != maxi.sharedArrayBuffers[v].rb.capacity) {
         for (let i = 0; i < avail; i += maxi.sharedArrayBuffers[v].blocksize) {
           let elements = new Float64Array(maxi.sharedArrayBuffers[v].blocksize);
-          let val = engine.sharedArrayBuffers[v].rb.pop(elements);
-          console.log(v,elements);
+          let val = maxi.sharedArrayBuffers[v].rb.pop(elements);
+          //console.log(v,elements[0]);
+          if(maxi.onData) {
+            maxi.onData(v,elements)
+          }
         }
       }
     }
-    setTimeout(sabPrinter, 100);
+    setTimeout(sabPrinter, 20);
   } catch (error) {
     // console.log(error);
-    setTimeout(sabPrinter, 100);
+    setTimeout(sabPrinter, 20);
   }
 }
 sabPrinter()
@@ -74,5 +88,7 @@ maxi.updateCode = (tag)=>{
   }`
   console.log(code)
   dspCode.setup = code;
-  maxi.eval(dspCode);
+  //with newer versions of the sema-engine.js, there needs to be a delay
+  //Between making the engine and updating the code. (13/7/21)
+  setTimeout(()=>{maxi.eval(dspCode)},200);
 }
--- emberShareDB/public/libs/transducers.js
+++ emberShareDB/public/libs/transducers.js
@@ -1,15 +1,19 @@
 import Maximilian from "./sema-engine.wasmmodule.js";
 import RingBuffer from "./ringbuf.js";
 // import { RingBuffer } from "ringbuf.js";
+
+
 export class SABInputTransducer {
 	constructor(id, triggered = 0) {
 		this.value = 0;
 		this.id = id;
 		this.triggered = triggered;
 		this.zx = new Maximilian.maxiTrigger();
+		console.log("SAB super CONSTRUCTOR")
 	}
 
 	getSABValue(inputBuffers, trigger) {
+		//console.log("getSABValue")
 		let reading = 1;
 		if (this.triggered) {
 			reading = this.zx.onZX(trigger);
@@ -24,12 +28,24 @@ export class SABInputTransducer {
 	}
 }
 
+export class Receiver extends SABInputTransducer {
+
+	constructor(id) {
+	  super(id);
+  }
+
+	getValue(inputBuffers, trigger) {
+		return this.getSABValue(inputBuffers, trigger)
+	}
+}
+
 export class SABOutputTransducer {
-	constructor(outputSABs, port, bufferType, channel, now, blocksize) {
+	constructor(outputSABs, port, ttype, channel, now, blocksize) {
 		this.port = port;
 		this.zx = new Maximilian.maxiTrigger();
 		this.channel = channel;
 		this.blocksize = blocksize;
+    this.ttype = ttype;
 
 		//check for existing channels
 		if (channel in outputSABs && outputSABs[channel].blocksize == blocksize) {
@@ -39,17 +55,18 @@ export class SABOutputTransducer {
 			//create a new SAB and notify the receiver
 			this.sab = RingBuffer.getStorageForCapacity(32 * blocksize, Float64Array);
 			this.ringbuf = new RingBuffer(this.sab, Float64Array);
-			outputSABs[channel] = {
+
+      outputSABs[channel] = {
 				rb: this.ringbuf,
 				sab: this.sab,
 				created: now,
 				blocksize: blocksize,
 			};
-
+			console.log("making SABOutputTransducer")
 			this.port.postMessage({
 				rq: "buf",
 				value: this.sab,
-				ttype: bufferType,
+				ttype: ttype,
 				channelID: channel,
 				blocksize: blocksize,
 			});
@@ -63,7 +80,7 @@ export class SABOutputTransducer {
 				if (typeof value == "number") {
 					this.ringbuf.push(new Float64Array([value]));
 				} else {
-					// console.log("SAB", value.length, this.blocksize);
+					//console.log("SAB", value.length, this.blocksize);
 					if (value.length == this.blocksize) {
 						this.ringbuf.push(value);
 					} else if (value.length < this.blocksize) {
@@ -74,9 +91,17 @@ export class SABOutputTransducer {
 						this.ringbuf.push(value.slice(0, this.blocksize));
 					}
 				}
-				// console.log('val written', value);
+				//console.log('val written', value, this.ringbuf.read_ptr[0], this.ringbuf.write_ptr[0]);
 			}
 		}
 		return value;
 	}
+}
+
+export class Sender extends SABOutputTransducer {
+
+	constructor(channel, blocksize, outputSABs, port) {
+	  super(outputSABs, port, 'ML', channel, 0, blocksize);
+  }
+
 }
