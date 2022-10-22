956a0260ebb2e1749354d38ab9732de847c9bf37
joshmurr <joshuadavidmurr@gmail.com>
2022-10-22 12:18:49 +0100

    Formatting on basic scene

--- emberShareDB/app/services/snippet.js
+++ emberShareDB/app/services/snippet.js
@@ -67,11 +67,32 @@ export default Service.extend({
         fn: () => ({
           snip: `
           <a-scene>
-            <a-box id="box_${randInt(4)}" position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-            <a-sphere id="sphere_${randInt(4)}" position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-            <a-cylinder id="cylinder_${randInt(4)}" position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
-            <a-plane id="plane_${randInt(4)}" position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-            <a-sky id="sky_${randInt(4)}" color="#ECECEC"></a-sky>
+            <a-box id="box_${randInt(4)}"
+                   position="-1 0.5 -3"
+                   rotation="0 45 0"
+                   color="#4CC3D9">
+            </a-box>
+            <a-sphere id="sphere_${randInt(4)}"
+                      position="0 1.25 -5"
+                      radius="1.25"
+                      color="#EF2D5E">
+            </a-sphere>
+            <a-cylinder id="cylinder_${randInt(4)}"
+                        position="1 0.75 -3"
+                        radius="0.5"
+                        height="1.5"
+                        color="#FFC65D">
+            </a-cylinder>
+            <a-plane id="plane_${randInt(4)}"
+                     position="0 0 -4"
+                     rotation="-90 0 0"
+                     width="4"
+                     height="4"
+                     color="#7BC8A4">
+            </a-plane>
+            <a-sky id="sky_${randInt(4)}"
+                   color="#ECECEC">
+            </a-sky>
           </a-scene>
           `,
           position: 'before',
